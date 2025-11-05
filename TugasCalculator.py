01ef57445e18ab588535fdd400ff7d512d36a46d
NaufalSalva <mhmmdnaufalh@gmail.com>
2025-11-05 12:49:16 +0700

    Second commit LTIK

---
+++ TugasCalculator.py
@@ -0,0 +1,4 @@
+Bilangan1 = float(input("Masukkan angka pertama: "))
+Bilangan2 = float(input("Masukkan angka kedua: "))
+Bilangan3 = float(input("Masukkan angka ketiga: "))
+print(f"Hasil penjumlahan {Bilangan1} + {Bilangan2} + {Bilangan3} = ", Bilangan1 + Bilangan2 + Bilangan3)
